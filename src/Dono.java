c1904ba339c1f5b49b0e1901d94ddc8183189a91
Carlos Eduardo Mendes de Mesquita <58157127+CarlosEduardoMendesdeMesquita@users.noreply.github.com>
2022-03-05 15:46:00 -0300

    Criado escopo da classe Dono(herdada de Pessoa)
    Criado apenas os escopos, funções ainda não adicionadas por completo e algumas coisas que devem fazer parte do final estão comentadas.

--- src/Dono.java
+++ src/Dono.java
@@ -1,12 +1,53 @@
-class Dono extends Pessoa {
-    String nome;
-    String cpf;
-    String endereco;
-    String telefone;
-    Animal animal;
+import java.util.Scanner;
 
-    Dono() {
-        System.out.println("Dono");
+public class Dono extends Pessoa{
+	
+	private String nome;
+	private String endereco;
+	//private Animal animais;
+	private int telefone;
+	private	int CPF;
+	//private int numeroAnimais;
+	
+	public Dono(String nome, String endereco,int telefone,int CPF) {
+		
+		this.nome = nome;
+		this.endereco = endereco;
+		//this.animais = animais;
+		this.telefone = telefone;
+		this.CPF = CPF;
+		//this.numeroAnimais = 0;//contagem de animais que esse dono possui
+		
+	}
+	
+	public String getNome(String nome){
+		
+		return nome;
+	}
 
-    }
+	public String getEndereco(String endereco){
+		
+		return endereco;
+	}
+	
+	public int getTelefone(int telefone){
+		
+		return telefone;
+	}
+	
+	public int getCPF(int CPF){
+		
+		return CPF;
+	}
+	
+	public void visualizarDonoCpf() {//lista o dono procurado por CPF e mostra os seus animais
+		
+	}
+	public void visualizarDonoNome() {//lista os possíveis donos procurados e ao escolher o que quer, mostra os seus animais
+		
+	}
+	
+	public void editarDono() {//edita os valores do dono cadastrado escolhido
+		
+	}
 }

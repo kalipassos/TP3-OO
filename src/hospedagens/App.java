aed92b569f556307503d6a5f54d384b67c96b569
Carlos Eduardo Mendes de Mesquita <58157127+CarlosEduardoMendesdeMesquita@users.noreply.github.com>
2022-03-08 17:34:13 -0300

    Alteração para que App rode a main do Menu.

--- src/hospedagens/App.java
+++ src/hospedagens/App.java
@@ -4,8 +4,10 @@ import java.text.ParseException;
 
 public class App {
     public static void main(String[] args) {
+        Menu menu = new Menu();
         try {
-            new Menu().inicio();
+            menu.inicio();
+            menu.menu();
         } catch (ParseException e) {
             e.printStackTrace();
         }

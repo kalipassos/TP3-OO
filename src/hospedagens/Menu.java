e7a15f20a10a6b6a7f05d7f3132c35bb06977a54
Carlos Eduardo Mendes de Mesquita <58157127+CarlosEduardoMendesdeMesquita@users.noreply.github.com>
2022-03-08 00:44:10 -0300

    Menus adicionados(métodos ainda em falta)

--- src/hospedagens/Menu.java
+++ src/hospedagens/Menu.java
@@ -14,6 +14,184 @@ import util.Scan;
 public class Menu {
         private final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
+        public void menu() {
+                int opcao;
+                do {
+                        System.out.println("Escolha a opcao que deseja executar:");
+                        System.out.println("1-Opcoes Dono:");
+                        System.out.println("2-Opcoes Anfitriao:");
+                        System.out.println("3-Opcoes Animal:");
+                        System.out.println("4-Opcoes Hospedagem:");
+                        System.out.println("5-Sair:");
+                        opcao = Scan.nextInt();
+                        switch (opcao) {
+                                case 1:
+
+                                        // opcaoDono;
+                                        break;
+                                case 2:
+
+                                        // opcaoAnfitriao;
+                                        break;
+                                case 3:
+
+                                        // opcaoAnimal;
+                                        break;
+                                case 4:
+
+                                        // opcaoHospedagem();
+                                        break;
+                                case 5:
+
+                                        System.out.println("Saindo...");
+
+                                        break;
+                                default:
+                                        System.out.println(
+                                                        "Numero invalido para uma opcao, escolha algum que leve a uma existente.");
+                                        break;
+                        }
+                } while (opcao != 5);
+        }
+
+        public void opcaoDono() {
+                int opcaoDono;
+                do {
+                        System.out.println("Opcoes para Dono sao:");
+                        System.out.println("1-Cadastrar Dono:");
+                        System.out.println("2-Visualizar Dono:");
+                        System.out.println("3-Editar Dono:");
+                        System.out.println("4-Deletar Dono:");
+                        System.out.println("5-Escolher outra opcao...");
+                        opcaoDono = Scan.nextInt();
+                        switch (opcaoDono) {
+                                case 1:
+                                        // cadastrarDono();
+                                        break;
+                                case 2:
+                                        // visualizarDono();
+                                        break;
+                                case 3:
+                                        // editarDono();
+                                        break;
+                                case 4:
+                                        // deletarDono();
+                                        break;
+                                case 5:
+
+                                        break;
+                                default:
+                                        System.out.println(
+                                                        "Numero invalido para uma opcao, escolha algum que leve a uma existente.");
+                                        break;
+                        }
+                } while (opcaoDono != 5);
+        }
+
+        public void opcaoAnfitriao() {
+                int opcaoAnfitriao;
+                do {
+                        System.out.println("Opcoes para Anfitriao sao:");
+                        System.out.println("1-Cadastrar Anfitriao:");
+                        System.out.println("2-Visualizar Anfitriao:");
+                        System.out.println("3-Editar Anfitriao:");
+                        System.out.println("4-Deletar Anfitriao:");
+                        System.out.println("5-Escolher outra opcao...");
+                        opcaoAnfitriao = Scan.nextInt();
+                        switch (opcaoAnfitriao) {
+                                case 1:
+                                        // cadastrarAnfitriao();
+                                        break;
+                                case 2:
+                                        // visualizarAnfitriao();
+                                        break;
+                                case 3:
+                                        // editarAnfitriao();
+                                        break;
+                                case 4:
+                                        // deletarAnfitriao();
+                                        break;
+                                case 5:
+
+                                        break;
+                                default:
+                                        System.out.println(
+                                                        "Numero invalido para uma opcao, escolha algum que leve a uma existente.");
+                                        break;
+                        }
+                } while (opcaoAnfitriao != 5);
+        }
+
+        public void opcaoAnimal() {
+                int opcaoAnimal;
+
+                do {
+                        System.out.println("Opcoes para Animal:");
+                        System.out.println("1-Cadastrar Animal:");
+                        System.out.println("2-Visualizar Animal:");
+                        System.out.println("3-Editar Animal:");
+                        System.out.println("4-Deletar Animal:");
+                        System.out.println("5-Escolher outra opcao...");
+                        opcaoAnimal = Scan.nextInt();
+                        switch (opcaoAnimal) {
+                                case 1:
+                                        // cadastrarAnimal;
+                                        break;
+                                case 2:
+                                        // visualizarAnimal;
+                                        break;
+                                case 3:
+                                        // editarAnimal;
+                                        break;
+                                case 4:
+                                        // deletarAnimal;
+                                        break;
+                                case 5:
+
+                                        break;
+                                default:
+                                        System.out.println(
+                                                        "Numero invalido para uma opcao, escolha algum que leve a uma existente.");
+                                        break;
+                        }
+                } while (opcaoAnimal != 5);
+        }
+
+        public void opcaoHospedagem() {
+                int opcaoHospedagem;
+
+                do {
+                        System.out.println("Opcoes para Hospedagem:");
+                        System.out.println("1-Cadastrar Hospedagem:");
+                        System.out.println("2-Visualizar Hospedagem:");
+                        System.out.println("3-Editar Hospedagem:");
+                        System.out.println("4-Deletar Hospedagem:");
+                        System.out.println("5-Escolher outra opcao...");
+                        opcaoHospedagem = Scan.nextInt();
+                        switch (opcaoHospedagem) {
+                                case 1:
+                                        // cadastrarHospedagem;
+                                        break;
+                                case 2:
+                                        // visualizarHospedagem;
+                                        break;
+                                case 3:
+                                        // editarHospedagem;
+                                        break;
+                                case 4:
+                                        // deletarHospedagem;
+                                        break;
+                                case 5:
+
+                                        break;
+                                default:
+                                        System.out.println(
+                                                        "Numero invalido para uma opcao, escolha algum que leve a uma existente.");
+                                        break;
+                        }
+                } while (opcaoHospedagem != 5);
+        }
+
         public void inicio() throws ParseException {
                 // var string = Scan.nextLine();
 

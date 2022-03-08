654b75e1b681b9103f3e9edee8c50d5c97812a45
Carlos Eduardo Mendes de Mesquita <58157127+CarlosEduardoMendesdeMesquita@users.noreply.github.com>
2022-03-08 19:15:50 -0300

    Adicionado controle de métodos do Anfitrião.

---
+++ src/controllers/AnfitriaoController.java
@@ -0,0 +1,30 @@
+package controllers;
+
+import model.Dono;
+import data.Dados;
+import util.Scan;
+import model.Hospedagem;
+import model.Anfitriao;
+
+public class AnfitriaoController {
+
+  public void CadastrarAnfitriao() {
+    String nome, endereco, telefone, CPF;
+
+    System.out.println("Nome do Anfitriao:");
+    nome = Scan.nextLine();
+
+    System.out.println("Endereco do Anfitriao:");
+    endereco = Scan.nextLine();
+
+    System.out.println("Telefone do Anfitriao:");
+    telefone = Scan.nextLine();
+
+    System.out.println("CPF do Anfitriao:");
+    CPF = Scan.nextLine();
+
+    Anfitriao anfitriao = new Anfitriao(nome, endereco, telefone, CPF);
+    Dados.getAnfitriao().add(anfitriao);
+  }
+
+}

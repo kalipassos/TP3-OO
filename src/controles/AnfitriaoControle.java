0fe36eb419e3f27ff88cbc0c2ae0694505790ff2
kazpmcd <kallyne.mcd@gmail.com>
2022-03-08 23:02:47 -0300

    Adição dos CRUDs de Anfitrião

--- src/controles/AnfitriaoControle.java
+++ src/controles/AnfitriaoControle.java
@@ -21,8 +21,125 @@ public class AnfitriaoControle {
     System.out.println("CPF do Anfitriao:");
     CPF = Scan.nextLine();
 
-    Anfitriao anfitriao = new Anfitriao(nome, endereco, telefone, CPF);
-    Dados.getAnfitriao().add(anfitriao);
+    System.out.println("Deseja cadastrar uma hospedagem? (s/n)");
+    String resposta = Scan.nextLine();
+
+    if (resposta == "s") {
+      System.out.println("Cadastre sua hospedagem agora! \n Data de Entrada:");
+      String dataEntrada = Scan.nextLine();
+
+      System.out.println("Data de Saida:");
+      String dataSaida = Scan.nextLine();
+
+      System.out.println("Animal que sera hospedado:");
+      String animal = Scan.nextLine();
+
+      System.out.println("Informacao adicional:");
+      String informacao = Scan.nextLine();
+
+      System.out.println("Cadastre um pagamento:");
+      String respostaPagamento = Scan.nextLine();
+
+      System.out.println("Forma de Pagamento:");
+      String formaPagamento = Scan.nextLine();
+
+      System.out.println("Valor do Pagamento:");
+      double valorPagamento = Scan.nextDouble();
+
+      Anfitriao anfitriao = new Anfitriao(nome, endereco, telefone, CPF);
+      anfitriao.getHospedagem();
+      // .add(new Hospedagem(dataEntrada, dataSaida, informacao, respostaPagamento,
+      // animal,
+      // .add(new Pagamento(formaPagamento, valorPagamento);
+      Dados.getAnfitriao().add(anfitriao);
+    } else {
+      Anfitriao anfitriao = new Anfitriao(nome, endereco, telefone, CPF);
+      Dados.getAnfitriao().add(anfitriao);
+    }
+
   }
 
+  public Anfitriao editarAnfitriao() {
+    String nome, endereco, telefone, CPF;
+    System.out.println("Digite o nome do Anfitriao que deseja editar:");
+    var anf1 = Scan.nextLine();
+    for (Anfitriao anfitriao : Dados.getAnfitriao()) {
+      if (anfitriao.getNome() != anf1) {
+        System.out.println("Anfitriao nao encontrado!");
+        return null;
+      }
+
+      if (anfitriao.getNome().equals(anf1)) {
+        System.out.println(anfitriao);
+        System.out.println("O que deseja editar no dono?");
+        System.out.println("1 - Nome");
+        System.out.println("2 - Endereco");
+        System.out.println("3 - Telefone");
+        System.out.println("4 - CPF");
+        var edit = Scan.nextInt();
+
+        // estrutura if-else para editar o atributo escolhido pelo usuario
+        if (edit == 1) {
+          System.out.println("Digite o novo nome:");
+          nome = Scan.nextLine();
+          anfitriao.setNome(nome);
+          System.out.println("Nome editado para " + anfitriao.getNome());
+        } else if (edit == 2) {
+          System.out.println("Digite o novo endereco:");
+          endereco = Scan.nextLine();
+          anfitriao.setEndereco(endereco);
+          System.out.println("Endereco editado para " + anfitriao.getEndereco());
+        } else if (edit == 3) {
+          System.out.println("Digite o novo telefone:");
+          telefone = Scan.nextLine();
+          anfitriao.setTelefone(telefone);
+          System.out.println("Telefone editado para " + anfitriao.getTelefone());
+        } else if (edit == 4) {
+          System.out.println("Digite o novo CPF:");
+          CPF = Scan.nextLine();
+          anfitriao.setCPF(CPF);
+          System.out.println("CPF editado para " + anfitriao.getCPF());
+        }
+      }
+      return anfitriao;
+    }
+
+    return null;
+
+  }
+
+  public Anfitriao deletarAnfitriao() {
+    String nome;
+    System.out.println("Digite o nome do Anfitriao que deseja deletar:");
+    nome = Scan.nextLine();
+
+    for (Anfitriao anfitriao : Dados.getAnfitriao()) {
+      if (anfitriao.getNome().equals(nome)) {
+        System.out.println("Anfitriao encontrado!");
+        System.out.println(anfitriao);
+        System.out.println("Deseja deletar o Anfitriao? (s/n)");
+        var resposta = Scan.nextLine();
+        if (resposta == "s") {
+          Dados.getAnfitriao().remove(anfitriao);
+          System.out.println("Anfitriao deletado com sucesso!");
+          return anfitriao;
+        } else {
+          System.out.println("Anfitriao nao deletado!");
+        }
+        if (anfitriao.getNome() != nome) {
+          System.out.println("Anfitriao nao encontrado!");
+          return null;
+        }
+      }
+    }
+    return null;
+
+  }
+
+  public void listarAnfitriao() {
+    for (Anfitriao anfitriao : Dados.getAnfitriao()) {
+      System.out.println("Esses sao os Anfitrioes cadastrados:");
+      System.out.println(anfitriao);
+    }
+  }
 }

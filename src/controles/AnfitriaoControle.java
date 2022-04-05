0d394d9b5ebb0177ff7722f169787d5bb06dddb7
kazpmcd <kallyne.mcd@gmail.com>
2022-04-05 10:20:56 -0300

    Adiçao de construtores

--- src/controles/AnfitriaoControle.java
+++ src/controles/AnfitriaoControle.java
@@ -9,6 +9,14 @@ import visualizacao.VisualAnfitriao;
 
 public class AnfitriaoControle {
 
+  private final Anfitriao visualizacao;
+  private final Dados dados;
+
+  public AnfitriaoControle(Anfitriao visualizacao, Dados dados) {
+    this.visualizacao = visualizacao;
+    this.dados = dados;
+  }
+
   public void cadastroAnfitriao(JButton botaoSelecionado) {
     String nome, endereco, telefone, CPF;
 
@@ -28,47 +36,6 @@ public class AnfitriaoControle {
   }
 
   public Anfitriao editaAnfitriao() { // editar atributo de um anfitriao
-    String nome, endereco, telefone, CPF;
-    System.out.println("Digite o nome do Anfitriao que deseja editar:");
-    var anf1 = Scan.nextLine();
-    for (Anfitriao anfitriao : Dados.getAnfitriao()) {
-      if (anfitriao.getNome().equals(anf1)) {
-        System.out.println(anfitriao);
-        System.out.println("O que deseja editar no dono?");
-        System.out.println("1 - Nome");
-        System.out.println("2 - Endereco");
-        System.out.println("3 - Telefone");
-        System.out.println("4 - CPF");
-        var edit = Scan.nextInt();
-
-        // estrutura if-else para editar o atributo escolhido pelo usuario
-        if (edit == 1) {
-          System.out.println("Digite o novo nome:");
-          nome = Scan.nextLine();
-          anfitriao.setNome(nome);
-          System.out.println("Nome editado para " + anfitriao.getNome());
-        } else if (edit == 2) {
-          System.out.println("Digite o novo endereco:");
-          endereco = Scan.nextLine();
-          anfitriao.setEndereco(endereco);
-          System.out.println("Endereco editado para " + anfitriao.getEndereco());
-        } else if (edit == 3) {
-          System.out.println("Digite o novo telefone:");
-          telefone = Scan.nextLine();
-          anfitriao.setTelefone(telefone);
-          System.out.println("Telefone editado para " + anfitriao.getTelefone());
-        } else if (edit == 4) {
-          System.out.println("Digite o novo CPF:");
-          CPF = Scan.nextLine();
-          anfitriao.setCPF(CPF);
-          System.out.println("CPF editado para " + anfitriao.getCPF());
-        } else if (anf1 != anfitriao.getNome()) {
-          System.out.println("Anfitriao nao encontrado");
-        }
-
-      }
-    }
-    return null;
   }
 
   public void deletaAnfitriao(JButton botaoSelecionado) {
@@ -109,20 +76,3 @@ public class AnfitriaoControle {
       System.out.println(anfitriao);
     }
   }
-
-  public Anfitriao visualizaAnfitriaoCpf() { // metodo para visualizar um dono pelo CPF
-    String buscaCPF;
-    System.out.println("Digite o CPF no modelo xxx.xxx.xxx-xx:");
-    buscaCPF = Scan.nextLine();
-
-    for (Anfitriao anfitriao : Dados.getAnfitriao()) {
-      if (anfitriao.getCPF().equals(buscaCPF)) {
-        System.out.println(anfitriao);
-        return anfitriao; // retorna o dono encontrado
-      }
-    }
-    System.out.println("CPF nao cadastrado.");
-    return null; // retorna null caso nao encontre o CPF
-  }
-
-}

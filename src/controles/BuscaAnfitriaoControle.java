c5180281dc6bb113ccbab12c6e1b6828526ed1c2
Carlos Eduardo Mendes de Mesquita <58157127+CarlosEduardoMendesdeMesquita@users.noreply.github.com>
2022-04-07 23:33:47 -0300

    Busca de Anfitrião.
    Busca de anfitrião.

--- src/controles/BuscaAnfitriaoControle.java
+++ src/controles/BuscaAnfitriaoControle.java
@@ -1,8 +1,5 @@
 package controles;
 
-import javax.swing.JButton;
-import javax.swing.JTextField;
-
 import data.Dados;
 import modelos.Anfitriao;
 import visualizacao.AnfitriaoVisual;
@@ -20,71 +17,57 @@ public class BuscaAnfitriaoControle {
 		this.visualizacao = visualizacao;
 	}
 
-	public Anfitriao buscador(JButton botaoSelecionado) {
-		for (Anfitriao anft : Dados.getAnfitriao()) {
-			if (anft.getNome().equalsIgnoreCase(visualizacao.getTextoNomeAnfitriao().getText())) {
-				return anft;
-			}
-		}
-		return null;
-
-	}
-
 	public void executarBotao(Object source) {
 		if (source == visualizacao.getBotaoBuscarAnfitriao()) {
-			Anfitriao achado = buscador(visualizacao.getBotaoBuscarAnfitriao());
-			if (achado != null) {
-				setField(visualizacao.getTextoEncontrado(), achado.getNome());
-				visualizacao.getRotuloEncontrado().setVisible(true);
-				visualizacao.getRotuloProximaAcao().setVisible(true);
-				visualizacao.getBotaoVisualizarAnfitriao().setVisible(true);
-				visualizacao.getBotaoEditarAnfitriao().setVisible(true);
-				visualizacao.getBotaoDeletarAnfitriao().setVisible(true);
-				visualizacao.getBotaoVoltar().setVisible(true);
-
-				visualizacao.getRotuloNaoEncontrado().setVisible(false);
-				visualizacao.getBotaoSim().setVisible(false);
-				visualizacao.getBotaoNao().setVisible(false);
-
-			} else {
-				visualizacao.getRotuloNaoEncontrado().setVisible(true);
-				visualizacao.getBotaoSim().setVisible(true);
-				visualizacao.getBotaoNao().setVisible(true);
-
-				visualizacao.getRotuloEncontrado().setVisible(false);
-				visualizacao.getRotuloProximaAcao().setVisible(false);
-				visualizacao.getBotaoVisualizarAnfitriao().setVisible(false);
-				visualizacao.getBotaoEditarAnfitriao().setVisible(false);
-				visualizacao.getBotaoDeletarAnfitriao().setVisible(false);
-				visualizacao.getBotaoVoltar().setVisible(false);
-			}
+			executeBuscaAfitriaoPeloNome(visualizacao.getTextoNomeAnfitriao().getText());
 		} else if (source == visualizacao.getBotaoSim()) {
-			new CadastrarAnfitriaoVisual();
+			new CadastrarAnfitriaoVisual().setVisible(true);
 			visualizacao.dispose();
 		} else if (source == visualizacao.getBotaoNao()) {
-			new AnfitriaoVisual();
+			new AnfitriaoVisual().setVisible(true);
 			visualizacao.dispose();
 		} else if (source == visualizacao.getBotaoVisualizarAnfitriao()) {
-			new VisualizaAnfitriaoVisual();
+			new VisualizaAnfitriaoVisual().setVisible(true);
 			visualizacao.dispose();
 		} else if (source == visualizacao.getBotaoEditarAnfitriao()) {
-			new EditarAnfitriaoVisual();
+			new EditarAnfitriaoVisual().setVisible(true);
 			visualizacao.dispose();
 		} else if (source == visualizacao.getBotaoDeletarAnfitriao()) {
-			new DeletaAnfitriaoVisual();
+			new DeletaAnfitriaoVisual().setVisible(true);
 			visualizacao.dispose();
 		} else if (source == visualizacao.getBotaoVoltar()) {
-			new AnfitriaoVisual();
+			new AnfitriaoVisual().setVisible(true);
 			visualizacao.dispose();
 		}
 	}
 
-	public static Anfitriao getAnfitriaoObj(int index) {
-		return Dados.getAnfitriao().get(index);
-	}
+	private void executeBuscaAfitriaoPeloNome(String nome) {
+		Anfitriao achado = Dados.getAnfitriaoPeloNome(nome);
 
-	public static void setField(JTextField field, String parametro) {
-		field.setText(parametro);
+		if (achado != null) {
+			visualizacao.getTextoEncontrado().setText(achado.getNome());
+			visualizacao.getRotuloEncontrado().setVisible(true);
+			visualizacao.getRotuloProximaAcao().setVisible(true);
+			visualizacao.getBotaoVisualizarAnfitriao().setVisible(true);
+			visualizacao.getBotaoEditarAnfitriao().setVisible(true);
+			visualizacao.getBotaoDeletarAnfitriao().setVisible(true);
+			visualizacao.getBotaoVoltar().setVisible(true);
+			visualizacao.getTextoEncontrado().setVisible(true);
 
+			visualizacao.getRotuloNaoEncontrado().setVisible(false);
+			visualizacao.getBotaoSim().setVisible(false);
+			visualizacao.getBotaoNao().setVisible(false);
+		} else {
+			visualizacao.getRotuloNaoEncontrado().setVisible(true);
+			visualizacao.getBotaoSim().setVisible(true);
+			visualizacao.getBotaoNao().setVisible(true);
+
+			visualizacao.getRotuloEncontrado().setVisible(false);
+			visualizacao.getRotuloProximaAcao().setVisible(false);
+			visualizacao.getBotaoVisualizarAnfitriao().setVisible(false);
+			visualizacao.getBotaoEditarAnfitriao().setVisible(false);
+			visualizacao.getBotaoDeletarAnfitriao().setVisible(false);
+			visualizacao.getBotaoVoltar().setVisible(false);
+		}
 	}
 }
